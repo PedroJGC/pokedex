a8d94d96d14af1bed4c5abb07362baa8e3b9bc62
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-16 21:36:18 -0300

    feat: testing the request

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import { Post } from "./components/Post"
+import { useEffect, useState } from "react"
+
+interface Pokemon {
+  name: string
+  image: string
+}
 
 function App() {
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null)
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon/pikachu"
+        )
+        const data = await response.json()
+
+        setPokemon({
+          name: data.name,
+          image: data.sprites.front_default,
+        })
+      } catch (error) {
+        console.error("Erro ao buscar Pokémon:", error)
+      }
+    }
+
+    fetchPokemon()
+  }, [])
+
+  if (!pokemon) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div>
-      <Post
-        author="Pedro Carvalho"
-        content="“Nutra sua mente com grandes pensamentos, pois você nunca irá mais alto do que o que você pensa.”"
-      />
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.image} alt={pokemon.name} />
     </div>
   )
 }
