b79384b557e113844894b59f506236c9ac203aec
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-26 21:58:05 -0300

    feat: implement DefaultLayout for improved routing structure and enhance Home page display

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom"
-import { Home } from "./pages/Home"
+import { DefaultLayout } from "./layouts/DefaultLayout"
 import { LoadPokemon } from "./pages/LoadPokemon"
+
+import { Home } from "./pages/Home"
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/load-pokemon" element={<LoadPokemon />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/load-pokemon" element={<LoadPokemon />} />
+      </Route>
     </Routes>
   )
 }
