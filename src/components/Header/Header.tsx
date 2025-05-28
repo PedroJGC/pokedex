7442579f321a38449c6618959920e956b69db37d
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-27 22:30:57 -0300

    feat: update index.html title and favicon, add search input in Header component with icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,27 @@
+import { MagnifyingGlassIcon } from "@phosphor-icons/react"
+import pokeballIcon from "../../assets/images/pokeball.svg"
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <header>
+      <div className="flex items-center justify-between mx-5 py-12 lg:mx-8 xl:mx-16 lg:py-8">
+        <img src={pokeballIcon} alt="" className="w-12" />
+        <div className="flex items-center justify-center relative ">
+          <input
+            className="placeholder:text-gray-500 placeholder:italic border border-gray-300 focus:border-rose-600 focus:outline-none focus:border-2 rounded-lg py-1 px-4"
+            type="text"
+            placeholder="Pesquise um Pokémon"
+          />
+          <div className="absolute right-2 cursor-pointer">
+            <MagnifyingGlassIcon size={24} />
+          </div>
+        </div>
+        <a href="#">
+          <h1 className="font-nunito text-slate-700 text-lg font-bold col-span-2">
+            Pokédex
+          </h1>
+        </a>
+      </div>
+    </header>
+  )
 }
